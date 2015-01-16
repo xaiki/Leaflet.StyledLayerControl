505b5252ee179e96a85483de35204f563b90a981
Niv Sardi <xaiki@evilgiggle.com>
2015-01-16 04:19:26 -0300

    smaller summary
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
         name: 'xaiki:leaflet-styledlayercontrol',
-        summary: "A Leaflet plugin that implements the management and control of layers by organization into categories or groups. The StyledLayerControl class extends the original L.control.layers control.",
+        summary: "A Leaflet plugin that implements the management and control of layers by organization into categories or groups.",
         version: '1.0.0',
         git: 'https://github.com/xaiki/Leaflet.StyledLayerControl'
 });
